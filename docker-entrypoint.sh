5ea497f97b7648808634f6db2f2759688d3b5071
VD3baeKy <digfehc@gmail.com>
2025-06-07 07:24:06 +0900

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
-# サービス起動
+# デフォルト値80、指定があれば${PORT}を使う
+export PORT=${PORT:-80}
+
+# テンプレートからnginx設定生成
+envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# 各サービス起動
+service mysql start
 service php8.2-fpm start
 nginx -g "daemon off;"
